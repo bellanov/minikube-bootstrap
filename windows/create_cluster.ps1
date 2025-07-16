c6e93d34c8da1d26fd8e2ce797511d73a0f2d54f
Bellanov Apilli <bellanovapilli@gmail.com>
2025-07-16 09:36:04 -0400

    Initial Windows Scripts

---
+++ windows/create_cluster.ps1
@@ -0,0 +1,9 @@
+# Create a Minikube Cluster.
+
+# Cluster Configuration
+$clusterName = "hello-world"
+$nodes = 2
+$driver = "docker"
+
+# Create Cluster
+minikube start --nodes $nodes -p $clusterName --driver=$driver
---
+++ windows/delete_cluster.ps1
@@ -0,0 +1,7 @@
+# Delete a Minikube Cluster.
+
+# Cluster Configuration
+$clusterName = "hello-world"
+
+# Delete a Minikube cluster
+minikube delete -p $clusterName
